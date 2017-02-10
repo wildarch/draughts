f91517f6f0062c98a919b896bb3297a80b563790
wildarch <daandegraaf9@gmail.com>
2017-02-10 16:31:24 +0100

    Initial source upload

---
+++ DraughtsPlugin/src/nl/tue/s2id90/group19/AIStoppedException.java
@@ -0,0 +1,7 @@
+package nl.tue.s2id90.group19;
+
+/**
+ *
+ * @author huub
+ */
+public class AIStoppedException extends Exception { }
---
+++ DraughtsPlugin/src/nl/tue/s2id90/group19/MyDraughtsPlugin.java
@@ -0,0 +1,29 @@
+package nl.tue.s2id90.group19;
+
+import nl.tue.s2id90.group19.samples.UninformedPlayer;
+import nl.tue.s2id90.group19.samples.OptimisticPlayer;
+import nl.tue.s2id90.group19.samples.BuggyPlayer;
+import net.xeoh.plugins.base.annotations.PluginImplementation;
+import nl.tue.s2id90.draughts.DraughtsPlayerProvider;
+import nl.tue.s2id90.draughts.DraughtsPlugin;
+
+
+
+/**
+ *
+ * @author huub
+ */
+@PluginImplementation
+public class MyDraughtsPlugin extends DraughtsPlayerProvider implements DraughtsPlugin {
+    public MyDraughtsPlugin() {
+        // make one or more players available to the AICompetition tool
+        // During the final competition you should make only your 
+        // best player available. For testing it might be handy
+        // to make more than one player available.
+        super(new MyDraughtsPlayer(5),
+                new UninformedPlayer(),
+                new OptimisticPlayer(),
+                new BuggyPlayer()
+        );
+    }
+}
