2a17b4e8af0edb9813d0af171a105f20721c0fce
Poels <y.r.j.poels@student.tue.nl>
2017-02-17 13:50:52 +0100

    Revert "Now checks if the current player is white, mirrors the score if not (it used to always assume the player is white, which isn't true)"
    This reverts commit d3e5688d9d636dd8459b63c79526c8a3b262f152.

--- DraughtsPlugin/src/nl/tue/s2id90/group19/MyDraughtsPlayer.java
+++ DraughtsPlugin/src/nl/tue/s2id90/group19/MyDraughtsPlayer.java
@@ -20,9 +20,6 @@ public class MyDraughtsPlayer  extends DraughtsPlayer{
     
     /** boolean that indicates that the GUI asked the player to stop thinking. */
     private boolean stopped;
-    
-    /** boolean that indicates the color of this player */
-    private static boolean isWhite;
 
     public MyDraughtsPlayer(int maxSearchDepth) {
         super("smiley.png"); // ToDo: replace with your own icon
@@ -31,8 +28,6 @@ public class MyDraughtsPlayer  extends DraughtsPlayer{
     }
     
     @Override public Move getMove(DraughtsState s) {
-        isWhite = s.isWhiteToMove(); //current player must be white
-        
         Move bestMove = null;
         bestValue = -7777777;
         DraughtsNode node = new DraughtsNode(s);    // the root of the search tree
@@ -201,7 +196,7 @@ public class MyDraughtsPlayer  extends DraughtsPlayer{
     
     /** A method that evaluates the given state. */
     int evaluate(DraughtsState s) {
-        return evaluate(s, true);
+        return evaluate(s, false);
     }
     
     public static int evaluate(DraughtsState state, boolean verbose) {
@@ -228,11 +223,6 @@ public class MyDraughtsPlayer  extends DraughtsPlayer{
         }
         int score;
         score = whites - blacks + 10*(whiteKings - blackKings);
-        
-        if (!isWhite) {
-            score *= -1;
-        }
-        
         if (verbose) {
             System.out.println("w: "+whites);
             System.out.println("b: "+blacks);
