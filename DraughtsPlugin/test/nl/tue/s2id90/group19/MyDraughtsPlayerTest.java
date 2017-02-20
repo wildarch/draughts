fd41daa166b48f87d9fe9988f34c939ba2307381
Yoeri Poels <yoeri.poels@hotmail.com>
2017-02-20 16:43:43 +0100

    Test case now uses a timelimit rather than going on forever.

--- DraughtsPlugin/test/nl/tue/s2id90/group19/MyDraughtsPlayerTest.java
+++ DraughtsPlugin/test/nl/tue/s2id90/group19/MyDraughtsPlayerTest.java
@@ -1,5 +1,7 @@
 package nl.tue.s2id90.group19;
 
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import nl.tue.s2id90.draughts.DraughtsState;
@@ -15,14 +17,31 @@ import org10x10.dam.game.Move;
  */
 public class MyDraughtsPlayerTest {
     
+    /** Timelimit per move */
+    int timelimit = 2;
+    
     public MyDraughtsPlayerTest() {
     }
     
     private boolean winsOf(DraughtsPlayer p1, DraughtsPlayer p2) {
+        Timer timer = new Timer();  
         DraughtsState ds = new DraughtsState();
         while(!ds.isEndState()) {
             DraughtsPlayer p = ds.isWhiteToMove()? p1 : p2;
-            ds.doMove(p.getMove(ds.clone()));
+            
+            /** Make sure the move does not exceed the time limit */
+            TimerTask task = new TimerTask() {
+                @Override
+                public void run() {
+                        p.stop();
+                }            
+           };
+           timer.schedule(task, timelimit * 1000);      
+           
+           /** Get best move and do it */
+           Move currentMove = p.getMove(ds.clone());
+           System.out.println(currentMove.getFullNotation());
+           ds.doMove(currentMove);
         }
         return !ds.isWhiteToMove();
     }
